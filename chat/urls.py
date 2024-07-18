25ecd0cd6897e03004872d37b7942ed7cce090f6
Humed-Muhammad <abc45171@gmail.com>
2024-07-18 22:19:30 +0300

    Hotfix

--- chat/urls.py
+++ chat/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import consumer, getUsers, login, getChatsByUser, uploadFileToS3
+from .views import getUsers, login, getChatsByUser, uploadFileToS3
 
 urlpatterns = [
     path('login', login.login_view, name='login'),
